b3d9a794d06617bba43d0b9acdb91ab19607cdf2
Emilia Pavlova <emilia.pavlova@gmail.com>
2016-06-21 20:53:55 +0300

    Added C# Basics exams

---
+++ C#Basics_March2016/Exams/2016-2017/25 April 2016 Evening/1. Feathers/Feathers.cs
@@ -0,0 +1,33 @@
+﻿namespace Feathers
+{
+    using System;
+
+    class Feathers
+    {
+        static void Main(string[] args)
+        {
+            int birdsInForest = int.Parse(Console.ReadLine());
+            int feathersInForest = int.Parse(Console.ReadLine());
+
+            double averageFeathersCountPerBird = (double)feathersInForest / birdsInForest;
+            if (birdsInForest == 0)
+            {
+                averageFeathersCountPerBird = 0;
+            }
+            else
+            {
+                if (birdsInForest % 2 == 0)
+                {
+                    averageFeathersCountPerBird *= 123123123123;
+                }
+                else
+                {
+                    averageFeathersCountPerBird /= 317;
+                }
+            }
+            
+
+            Console.WriteLine("{0:F4}", averageFeathersCountPerBird);
+        }
+    }
+}
---
+++ C#Basics_March2016/Exams/2016-2017/25 April 2016 Evening/2. Busses/Busses.cs
@@ -0,0 +1,27 @@
+﻿namespace Busses
+{
+    using System;
+
+    class Busses
+    {
+        static void Main(string[] args)
+        {
+            int c = int.Parse(Console.ReadLine());
+            int groups = 1;
+            int groupSpeed = int.Parse(Console.ReadLine()); 
+
+            for (int i = 1; i < c; i++)
+            {
+                int speed = int.Parse(Console.ReadLine());
+
+                if(speed <= groupSpeed)
+                {
+                    groupSpeed = speed;
+                    groups++;
+                }
+            }
+
+            Console.WriteLine(groups);
+        }
+    }
+}

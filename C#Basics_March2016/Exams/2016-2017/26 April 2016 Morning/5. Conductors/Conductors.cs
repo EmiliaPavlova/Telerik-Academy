c5bf2faf57460ff5e012065eb96282383458983d
Emilia Pavlova <emilia.pavlova@gmail.com>
2016-07-15 23:35:39 +0300

    Added last task solution

--- C#Basics_March2016/Exams/2016-2017/26 April 2016 Morning/5. Conductors/Conductors.cs
+++ C#Basics_March2016/Exams/2016-2017/26 April 2016 Morning/5. Conductors/Conductors.cs
@@ -1,15 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Conductors
+﻿namespace Conductors
 {
-    class Program
+    using System;
+
+    class Conductors
     {
         static void Main(string[] args)
         {
+            int p = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int length = Convert.ToString(p, 2).Length;
+            int perforator = (1 << length) - 1 ;
+
+
+            for (int i = 0; i < m; i++)
+            {
+                int n = int.Parse(Console.ReadLine());
+                int pattern = p;
+                int mask = perforator;
+
+                do
+                {
+                    if ((n & mask) == pattern)
+                    {
+                        n &= ~mask;
+                    }
+
+                    mask <<= 1;
+                    pattern <<= 1;
+                } while ((pattern & (1 << 31)) == 0);
+
+                Console.WriteLine(n);
+            }
         }
     }
 }
